3a29b09e64c8a4deb9fa35ea78a45a701c31ca47
vinhblue97 <vinhblue97@gmail.com>
2020-08-13 08:30:31 +0700

    8:30 13/08/2020

--- Module_2/src/dsa_list/exercise/linked_list_by_java_util_linked_list/MyLinkedList.java
+++ Module_2/src/dsa_list/exercise/linked_list_by_java_util_linked_list/MyLinkedList.java
@@ -1,52 +1,15 @@
 package dsa_list.exercise.linked_list_by_java_util_linked_list;
 
+import dsa_list.exercise.array_list_by_java_util_arraylist.MyList;
+
 public class MyLinkedList {
     private Node head;
-    private int numNodes;
+    private Node tail;
+    private int size = 0;
 
-    public MyLinkedList(Object data) {
-        head = new Node(data);
-        System.out.println(head);
-    }
+    public MyLinkedList(){
+        head = null;
+        tail = null;
 
-    private class Node {
-        private Node next;
-        private Object data;
-
-        public Node(Object data) {
-            this.data = data;
-        }
-
-        public Object getData() {
-            return this.data;
-        }
-    }
-
-    public void add(int index, Object data) {
-        Node temp = head;
-        Node holder;
-        for (int i = 0; i < index - 1 && temp.next != null; i++) {
-            temp = temp.next;
-        }
-        holder = temp.next;
-        temp.next = new Node(data);
-        System.out.println(temp.next);
-        temp.next.next = holder;
-        numNodes++;
-    }
-
-    public void addFirst(Object data) {
-        Node temp = head;
-        head = new Node(data);
-        head.next = temp;
-        numNodes++;
-    }
-
-    public void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data+" ");
-            temp = temp.next;
-        }
     }
 }
