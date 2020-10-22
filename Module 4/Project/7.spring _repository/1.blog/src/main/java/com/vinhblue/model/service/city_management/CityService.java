e4878bd67a4fd4f1eb397ddb6ac77a28eed17d0c
Your Name <you@example.com>
2020-10-22 17:16:30 +0700

    22/10/2020

---
+++ Module 4/Project/7.spring _repository/1.blog/src/main/java/com/vinhblue/model/service/city_management/CityService.java
@@ -0,0 +1,4 @@
+package com.vinhblue.model.service.city_management;
+
+public interface CityService {
+}
---
+++ Module 4/Project/7.spring _repository/1.blog/src/main/java/com/vinhblue/model/service/city_management/impl/CityServiceImpl.java
@@ -0,0 +1,4 @@
+package com.vinhblue.model.service.city_management.impl;
+
+public class CityServiceImpl {
+}
